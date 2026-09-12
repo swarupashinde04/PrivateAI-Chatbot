f4bda64614a3ab603007ff10b2a483866f4539ac
Swarupa Shinde <swarupa8387@gmail.com>
2026-09-12 23:08:16 +0530

    Connect frontend to Cloudflare backend tunnel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 
-const API = "http://127.0.0.1:8000";
+const API = "https://tooth-korean-insider-disclose.trycloudflare.com";
 
 
 const uiStyles = `
